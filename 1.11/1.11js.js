1fc4a9a4a7685e2bb7b2cc291693075133300088
virsta <virstanastya21@gmail.com>
2021-02-25 18:10:43 +0200

    3

--- 1.11/1.11js.js
+++ 1.11/1.11js.js
@@ -23,3 +23,25 @@ let b = prompt("count");
 console.log(perimetr(a, b));
  */
 
+/*
+//3
+function output(n){
+    for(let a = 1; a <= n; a++){
+        if(a % 3 === 0 && a % 5 === 0){
+            console.log("fizzbuzz");
+        }
+        else if(a % 3 === 0){
+            console.log("fizz");
+        }
+        else if(a % 5 === 0){
+            console.log("buzz");
+        }
+        else {
+            console.log(a);
+        }
+    }
+}
+let a = prompt("n");
+console.log(output(a));
+ */
+
