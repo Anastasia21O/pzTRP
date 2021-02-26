b5aee02a10a58f5a38eaed803aafdc92180ad515
virsta <virstanastya21@gmail.com>
2021-02-27 00:33:41 +0200

    5

--- 1.13/1.13js.js
+++ 1.13/1.13js.js
@@ -125,3 +125,21 @@ console.log(g)
 console.log (g.greet("Hello"));
  */
 
+/*
+//5
+function sequence(a, b){
+    let start = a;
+    let krok = b;
+
+    return () => start += krok;
+}
+
+let generator = sequence(10, 3);
+let generator2 = sequence(7, 1);
+console.log(generator());
+console.log(generator());
+console.log(generator2());
+console.log(generator());
+console.log(generator2());
+ */
+
