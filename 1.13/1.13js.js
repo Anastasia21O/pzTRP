56b5ab57678ad6fd6d8e242380f98d49524707f9
virsta <virstanastya21@gmail.com>
2021-02-27 00:34:50 +0200

    7

--- 1.13/1.13js.js
+++ 1.13/1.13js.js
@@ -160,3 +160,20 @@ let characters = [
 console.log (pluck(characters, name)); //['barney', 'fred'];
  */
 
+/*
+//7
+function count(obj){
+    return Object.getOwnPropertyNames(obj);
+}
+
+let a = { a: 1, b: 2 };
+console.log(count(a));
+let b = function () {};
+console.log(count(b));
+let c = [1, 2, 3];
+console.log(count(c));
+let d = [];
+d[100] = 1;
+console.log(count(d));
+ */
+
